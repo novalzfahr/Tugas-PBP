cb59f7b3848aaad88d3f4c3cb00de0bdded6ffa3
novalzfahr <novalzfahr22@gmail.com>
2022-09-11 21:33:21 +0700

    push tugas

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+    'list_barang': data_barang_katalog,
+    'nama': 'Achmad Noval Fahrezi',
+    'student_ID': '2106750931'
+    }
+    return render(request, "katalog.html", context)
